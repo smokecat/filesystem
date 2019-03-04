1a325506e576c01071d94f2ead18c7b0ace06779
smokecat <smokecatyo@gmail.com>
2019-03-05 02:33:37 +0800

    update

--- src/com/smoke/filesystem/INode.java
+++ src/com/smoke/filesystem/INode.java
@@ -7,12 +7,22 @@ public class INode {
 	private int flag;			//	文件类型，0为空闲inode，1为文件夹，2为文件
 	private int owner;			//	文件所属者id,root用户为0
 	private int[] ptr;			//	指向存储数据块
+	private int[] pos;			//	此inode的位置信息{行，列}
 	
-	public INode(int i, int f, int o) {
+	public INode() {
+		id = 0;
+		flag = 0;
+		owner = 0;
+		ptr = new int[FileSystem.getInodeSize()-3];
+		pos = new int[2];
+	}
+	
+	public INode(int i, int f, int o, int[] p) {
 		id = i;
 		flag = f;
 		owner = o;
 		ptr = new int[FileSystem.getInodeSize()-3];
+		pos = p;
 	}
 	
 	public void setNextPtr(int ptrNo) {
@@ -67,6 +77,18 @@ public class INode {
 
 	public void setFlag(int flag) {
 		this.flag = flag;
+	}
+
+	public int[] getPos() {
+		return pos;
+	}
+
+	public void setPos(int[] pos) {
+		this.pos = pos;
+	}
+
+	public void setPtr(int[] ptr) {
+		this.ptr = ptr;
 	}
 	
 }
