15b66e8416f14f16f65030d00e286a9ede561b3e
smokecat <smokecatyo@gmail.com>
2019-03-05 01:31:59 +0800

    update

--- src/com/smoke/filesystem/INode.java
+++ src/com/smoke/filesystem/INode.java
@@ -1,8 +1,38 @@
 package com.smoke.filesystem;
 
-public class INode {
 
+public class INode {
 	
+	private int id;				//	inode号
+	private int flag;			//	文件类型，0为空闲inode，1为文件夹，2为文件
+	private int owner;			//	文件所属者id,root用户为0
+	private int[] ptr;			//	指向存储数据块
+	
+	public INode(int i, int f, int o) {
+		id = i;
+		flag = f;
+		owner = o;
+		ptr = new int[FileSystem.getInodeSize()-3];
+	}
+	
+	public void setNextPtr(int ptrNo) {
+		/*
+		 * 	设置文件存储块指针
+		 */
+		int i = 0;
+		while(ptr[i]!=0 && i<ptr.length) {
+			i++;
+		}
+		ptr[i] = ptrNo;
+	}
+	
+	public void freeINode(){
+		flag = 0;
+		owner = 0;
+		for(int i=0; i<ptr.length; i++) {
+			ptr[i] = 0;
+		}
+	}
 	
 	@Override
 	public String toString() {
@@ -10,7 +40,33 @@ public class INode {
 		
 		return iNodeStr;
 	}
-	
-	
+
+	public int getFlag() {
+		return flag;
+	}
+
+	public void setOwner(int owner) {
+		this.owner = owner;
+	}
+
+	public int[] getPtr() {
+		return ptr;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getOwner() {
+		return owner;
+	}
+
+	public void setFlag(int flag) {
+		this.flag = flag;
+	}
 	
 }
--- src/com/smoke/filesystem/INodeBlock.java
+++ src/com/smoke/filesystem/INodeBlock.java
@@ -5,6 +5,39 @@ public class INodeBlock {
 	
 	public INode[] iNodes = new INode[INODES_PER_BLOCK];
 	
+	public INodeBlock(String line) {
+		/*
+		 * 	构造
+		 */
+		String[] blockStr = line.split(" ");
+		if(blockStr.length!=64)
+			System.out.println("error: INodeBlock's length not equal to 64!");
+		int j=0;
+		for(int i=0; i<INODES_PER_BLOCK; i++) {
+			iNodes[i].setId(Integer.parseInt(blockStr[j++]));
+			iNodes[i].setFlag(Integer.parseInt(blockStr[j++]));
+			iNodes[i].setOwner(Integer.parseInt(blockStr[j++]));
+			for(int k=0; k<iNodes[i].getPtr().length; k++) {
+				iNodes[i].setNextPtr(Integer.parseInt(blockStr[j++]));
+			}
+		}
+	}
+	
+	public int[] searchNode(int  iid) {
+		/*
+		 * 	根据id查找iNode
+		 * 	返回包含两个int值的int数组
+		 * 	第一位，0表示没有查找的INode， 1表示查找成功，第二位是INode在此INodeBlock的序号
+		 */
+		for(int i=0; i<INODES_PER_BLOCK; i++) {
+			if(iNodes[i].getId() == iid) {
+				return new int[] {1,i};
+			}
+		}
+		
+		return new int[] {0,0};
+	}
+	
 	public void writeINodeBlock() {
 		
 	}
